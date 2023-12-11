68f8d9d585cac1b8728a907e75e154e323cd5f04
IrynaBarnych <iryna.barnych@gmail.com>
2023-12-11 19:52:13 +0200

    myzip

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ print(serialized_data)
 with open("serialized_data.pickle", "wb") as file:
     pickle.dump(data, file)
 
+
 #десеріалізація
 deserialized_data = pickle.loads(serialized_data)
 print(deserialized_data)
@@ -15,3 +16,29 @@ print(deserialized_data)
 with open("serialized_data.pickle", "rb") as file:
     deserialized_data_file = pickle.load(file)
 print(deserialized_data_file)
+
+### приклад стиснення інформації
+import gzip
+with gzip.open('compressed_file.gz', "wb") as file:
+    file.write(b"some text")
+
+#  ще один з видів стискання
+data = b"some text"
+compressed_data = gzip.compress(data)
+print(compressed_data)
+
+# розпакування стиснених даних
+decompressed_data = gzip.decompress(compressed_data)
+print(decompressed_data)
+
+#розпакування з файлу
+with gzip.open('compressed_file.gz', "rb") as file:
+    new_data = file.read()
+print(new_data)
+
+
+from zipfile import ZipFile
+with ZipFile('metanit.zip', "a") as myzip:
+    myzip.write("eggs.txt")
+    myzip.write("serialized_data.pickle")
+
